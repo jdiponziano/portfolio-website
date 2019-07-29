8fcd1addd8e61df9696d6cd8f8c641e768275c7a
Jessica DiPonziano <jess@webdevjd.com>
2019-07-29 16:29:47 -0400

    Add require data

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { data } = require('./data/portfolio-data.json');
+const { projects } = data;
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Portfolio site listening on port ${port}!`));
