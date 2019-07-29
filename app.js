b2de2f67de0112e2bc46fb01e17298978ef02d2e
Jessica DiPonziano <jess@webdevjd.com>
2019-07-29 16:31:58 -0400

    Initial setup of middleware

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 const { data } = require('./data/portfolio-data.json');
 const { projects } = data;
 
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Portfolio site listening on port ${port}!`));
