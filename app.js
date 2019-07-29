31c9b80b912ac7baf7dcb1ba630b88e9e2ab3e4e
Jessica DiPonziano <jess@webdevjd.com>
2019-07-29 17:21:14 -0400

    Fix typo on variable

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.get('/project/:id', (req, res) => {
   const { description } = projects[id];
   const { technologies } = projects[id];
   const { live_link } = projects[id];
-  const { github_links } = projects[id];
+  const { github_link } = projects[id];
   const { image_urls } = projects[id];
 
-  const templateData = { id, project_name, description, technologies, live_link, github_links, image_urls };
+  const templateData = { id, project_name, description, technologies, live_link, github_link, image_urls };
 
   res.render('project', templateData);
 });
