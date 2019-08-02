fd5a1e5391e6075a503a0e24034e3a200101a704
Jessica DiPonziano <jess@webdevjd.com>
2019-08-02 15:50:02 -0400

    Refactor and restructure

--- app.js
+++ app.js
@@ -2,51 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-
-const { data } = require('./data/portfolio-data.json');
-const { projects } = data;
+const mainRoutes = require('./routes');
+const projectRoutes = require('./routes/projects');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index', { projects });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/projects', (req, res) => {
-  const firstProject = projects[0].id;
-  res.redirect(`/project/${firstProject}`);
-});
-
-app.get('/project/:id', (req, res) => {
-  const { id } = req.params;
-
-  if (!id) {
-    const firstProject = projects[0].id;
-    return res.redirect(`/project/${firstProject}`)
-  }
-
-  const { project_name } = projects[id];
-  const { description } = projects[id];
-  const { technologies } = projects[id];
-  const { live_link } = projects[id];
-  const { github_link } = projects[id];
-  const { image_urls } = projects[id];
-
-  const templateData = { id, project_name, description, technologies, live_link, github_link, image_urls };
-
-  res.render('project', templateData);
-});
+app.use(mainRoutes);
+app.use(projectRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Sorry! This page does not exist!');
---
+++ routes/projects.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { data } = require('../data/portfolio-data.json');
+const { projects } = data;
+
+router.get('/projects', (req, res) => {
+  const firstProject = projects[0].id;
+  res.redirect(`/project/${firstProject}`);
+});
+
+router.get('/project/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    const firstProject = projects[0].id;
+    return res.redirect(`/project/${firstProject}`)
+  }
+
+  const { project_name } = projects[id];
+  const { description } = projects[id];
+  const { technologies } = projects[id];
+  const { live_link } = projects[id];
+  const { github_link } = projects[id];
+  const { image_urls } = projects[id];
+
+  const templateData = { id, project_name, description, technologies, live_link, github_link, image_urls };
+
+  res.render('project', templateData);
+});
+
+module.exports = router;
